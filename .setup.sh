de44ccbb93c5740fc45fc2cf92b0bb4c5766441d
Joe Polny <joepolny@gmail.com>
2020-01-19 19:52:19 +0000

    Converted to a single command

--- .setup.sh
+++ .setup.sh
@@ -1,8 +1,2 @@
-sudo dnf install git
-sudo dnf install vim
-sudo dnf install powerline powerline-fonts
-sudo dnf install vim-powerline
-sudo dnf install ranger
-sudo dnf install elixir
-sudo dnf install postgresql-server postgresql-contrib
+sudo dnf install git vim powerline powerline-fonts vim-powerline ranger elixir postgresql-server postgresql-contrib
 
