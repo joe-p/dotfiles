10fdbf180ba48e753db09f35a85fdb9b52386974
Joe Polny <joepolny@gmail.com>
2026-05-07 12:23:03 -0400

    change origin to ssh

--- .setup/setup-dotfiles.sh
+++ .setup/setup-dotfiles.sh
@@ -3,3 +3,6 @@
 git clone --bare https://github.com/joe-p/dotfiles.git $HOME/.dotfiles
 git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout -f
 git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME submodule update --init
+git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME remote remove origin
+git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME remote add origin git@github.com:joe-p/dotfiles.git
+
