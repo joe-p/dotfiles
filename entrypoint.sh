fb62a51131a0a262b1b8b5deb9b42e258913a719
Joe Polny <joepolny@gmail.com>
2026-05-18 20:44:39 -0400

    ports from env

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 HOST_IP=$(ip route | awk '/default/ {print $3; exit}')
-socat TCP-LISTEN:4001,bind=127.0.0.1,reuseaddr,fork TCP:"$HOST_IP":4001 &
-socat TCP-LISTEN:4002,bind=127.0.0.1,reuseaddr,fork TCP:"$HOST_IP":4002 &
-socat TCP-LISTEN:8980,bind=127.0.0.1,reuseaddr,fork TCP:"$HOST_IP":8980 &
 
+# Split on comma and start a socat forwarder for each port
+IFS=',' read -ra PORT_LIST <<< "$FORWARD_PORTS"
+for port in "${PORT_LIST[@]}"; do
+    # Trim whitespace
+    port="${port//[[:space:]]/}"
+    [ -z "$port" ] && continue
+    socat TCP-LISTEN:"$port",bind=127.0.0.1,reuseaddr,fork TCP:"$HOST_IP":"$port" &
+done
 
 if [ ! -d "$HOME/.dotfiles" ]; then
 	git clone --bare https://github.com/joe-p/dotfiles.git $HOME/.dotfiles
