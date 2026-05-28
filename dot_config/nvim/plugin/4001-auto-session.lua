4720e7efcd10362f36b00277755a5cdef032f37e
Joe Polny <joepolny@gmail.com>
2026-05-28 01:00:06 +0000

    search sessions keymap

--- dot_config/nvim/plugin/4001-auto-session.lua
+++ dot_config/nvim/plugin/4001-auto-session.lua
@@ -1,22 +1,24 @@
-vim.pack.add({ 'https://github.com/rmagatti/auto-session' })
+vim.pack.add { 'https://github.com/rmagatti/auto-session' }
 
-require('auto-session').setup({
+require('auto-session').setup {
   suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
   git_use_branch_name = false,
   git_auto_restore_on_branch_change = false,
   show_auto_restore_notif = true,
   pre_save_cmds = {
     function()
-      local dart = require('dart')
+      local dart = require 'dart'
       local name = require('auto-session.lib').current_session_name()
       dart.write_session(name:gsub('/', '%%'))
     end,
   },
   post_restore_cmds = {
     function()
-      local dart = require('dart')
+      local dart = require 'dart'
       local name = require('auto-session.lib').current_session_name()
       dart.read_session(name:gsub('/', '%%'))
     end,
   },
-})
+}
+
+vim.keymap.set('n', '<leader>ww', '<cmd>AutoSession search<cr>', { desc = 'search sessions' })
